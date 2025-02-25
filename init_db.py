1f24fb086262e68b90e8206682e90601e1921a50
Yavnik Nayka <yavnik.dds@gmail.com>
2025-02-25 17:45:04 +0530

    the app is instancieted in __init__.py so had to provide path of the template in that file but download functionality is not working may be for the same reason, may be i have to provide new path or change the app structure so that dont have to provide diffrent paths in to the app instance

--- init_db.py
+++
@@ -1,5 +0,0 @@
-from app import db, app
-
-with app.app_context():
-    db.create_all()
-    print("Database tables created successfully!")
--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import config
+import os
 
 db = SQLAlchemy()  # Initialize without app
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+    )
     app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)  # Bind db to app
